fa2e9f30da9478bbf9181d64df4e10e40908834a
Juliana Andrea Florido Manjarré <jafloridom@udistrital.edu.co>
2025-09-16 17:56:04 -0500

    v0.2

--- program.js
+++ program.js
@@ -1,18 +1,22 @@
 var map = L.map('map');
-map.setView([4.628024660207178, -74.06586550974782],15);
-var urlMap='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-var config={
-    maxZoim: 19
-}
+map.setView([4.628010767310762, -74.06580627094172], 15);
 
-var layer= L.titleLayer(urlMap, config);
+var urlMap = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+var config = {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' // Línea agregada
+};
+
+var layer = L.tileLayer(urlMap, config); // Corregido: L.titleLayer -> L.tileLayer
 layer.addTo(map);
 
-async function leerGeoJSON(url){
-    var response=fetch(url);
-    return (await response).json();
+async function leerGeoJSON(url) {
+    var response = await fetch(url); // Corregido: añadir await
+    return response.json();
 }
-var myfile = leerGeoJSON('map.geojson');
 
-const geoLayer = L.leerGeoJSON(myfile);
-geoLayer.addTo(map);
+leerGeoJSON('map.geojson').then(data => { // Corregido: manejo de promesas
+    const geoLayer = L.geoJSON(data); // Corregido: L.leerGeoJSON -> L.geoJSON
+    geoLayer.addTo(map);
+});
